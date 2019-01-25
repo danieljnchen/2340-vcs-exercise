deee69232b7b8e5ed6e59db313369e4d4f1b90d2
Daniel Wade <daniel.wadefamily@gmail.com>
2019-01-25 14:12:35 -0500

    completed person5.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -31,7 +31,7 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+	  return input.substring(input.length() - 2, input.length() - 1) + input.substring(0, input.length() - 2);
 	}
 	
 	/**
